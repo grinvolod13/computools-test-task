a293b15297d950d1310f203d417ec08cc6006853
grinvolod13 <grinvols@gmail.com>
2025-06-23 00:59:15 +0300

    add debug mode env variable reading

--- app/app.py
+++ app/app.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
+import app.dependency
 from app.endpoints import result_router
 
+if not app.dependency.DEBUG:
+    raise NotImplementedError("Feature is not ready for live yet")
+else:
+    # TODO: load data from test json to db
+    ...
 
 app = FastAPI()
 app.include_router(result_router, prefix="/results")
+
